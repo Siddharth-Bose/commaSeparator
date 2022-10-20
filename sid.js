0bbda0ee13dea5ac73485201df53112b4cdd1de7
Siddharth Bose <88647238+Siddharth-Bose@users.noreply.github.com>
2022-10-20 13:08:14 +0530

    Update sid.js

--- sid.js
+++ sid.js
@@ -1,17 +1,15 @@
-function separateByComma(){
+const separateByComma=()=>{
+
+    console.log("clcicked");
     listWithoutComma= [];
     listWithoutCommaFinal= [];
-    choice1=';';
+    choice1='\n';
     listWithoutComma.push(document.querySelector('#original').value.split(choice1));
-    for (var i=0;i<listWithoutComma[0].length;i++){
-        listWithoutCommaFinal.push(listWithoutComma[0][i]);
-        listWithoutCommaFinal=removeDuplicates(listWithoutCommaFinal);
-        choice2=',';
-        var text = listWithoutCommaFinal.join("'"+choice2+"'");
-        document.querySelector('#commaSeparated').value=text;
-        setTimeout(function() {document.querySelector('.fas').style="box-shadow:0 0 5px 5px grey;";},0);
-        setTimeout(function() {document.querySelector('.fas').style="box-shadow:0 0 0 0 grey;";},100);
-    }
+    finalList=removeDuplicates(listWithoutComma);
+    finalList.join(',');
+    document.querySelector('#commaSeparated').value=finalList;
+}
+{
 }
 function removeDuplicates(listWithoutCommaFinal){
     function removeIt(word){
